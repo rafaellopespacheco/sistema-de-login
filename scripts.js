12204e53034b4829501b9ddaa572fd0d62dc20bc
Rafael Lopes <80158191+rafaellopespacheco@users.noreply.github.com>
2024-12-28 00:38:28 -0300

    versao 2

--- scripts.js
+++ scripts.js
@@ -4,3 +4,18 @@ document.getElementById('fazerlogin').addEventListener('click', function () {
 
    alert(`O login que você enviou foi.\n Usuário: ${usuario}\n Senha: ${senha}`)
 });
+
+var senhaInput = document.getElementById('senha');
+var olhoIcon = document.querySelector('.olho-icon');
+function mostrarSenha() {
+   senhaInput.type = (senhaInput.type === 'password') ? 'text' : 'password';
+   olhoIcon.src = (senhaInput.type === 'password')
+      ? 'assets/olho_fechado.png'
+      : 'assets/olho_aberto.png';
+}
+senhaInput.addEventListener('input', function () {
+   olhoIcon.style.display = (senhaInput.value.length > 0) ? 'inline-block' : 'none';
+});
+window.addEventListener('load', function () {
+   olhoIcon.style.display = (senhaInput.value.length > 0) ? 'inline-block' : 'none';
+});
